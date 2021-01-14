a05df49b2d1d0625cb83a4b5957c3b84bc43e84a
eeezi <76932879+eeezi@users.noreply.github.com>
2021-01-14 09:40:28 +0900

    Update C언어콘서트_chap4-5.c

--- C언어콘서트_chap4-5.c
+++ C언어콘서트_chap4-5.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
+double cal(double x);
+
 int main(void) {
 	double x, result;
 
-	printf("수식: f(x) = (x ^ 3 - 20) / (x - 7)");
-	printf("정수를 입력하시오");
-	scanf("%d", &x);
-	result = (pow(x, 3) - 20) / (x - 7);
+	printf("정수를 입력하시오 ");
+	scanf("%lf", &x);
+	result = cal(x);
 	printf("수식의 값은 %lf입니다.", result);
 
+}
+
+double cal(double x) {
+	return (pow(x, 3) - 20) / (x - 7);
 }
