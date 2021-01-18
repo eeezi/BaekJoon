81fd76d4b8e5c56f24e7a6654e792e02ab5e472c
eeezi <76932879+eeezi@users.noreply.github.com>
2021-01-18 19:47:05 +0900

    시작

--- BAEKJOON_2442.c
+++ BAEKJOON_2442.c
@@ -6,7 +6,7 @@ int main(void) {
 	
 	for (int i = 0; i < n; i++) {
 		for (int k = n - 1; k > i; k--)
-			printf(" ");
+			printf("  ");
 		for (int j = 0; j < 2 * i + 1; j++)
 			printf("*");
 		printf("\n");
