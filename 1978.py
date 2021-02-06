6af44ddebe673ff2d3a3f211bc7e1ee69c889e7d
eeezi <76932879+eeezi@users.noreply.github.com>
2021-02-06 23:21:06 +0900

    소수 찾기

---
+++ 1978.py
@@ -0,0 +1,15 @@
+n = int(input())
+num = list(map(int, input().split()))
+num_count = 0
+
+for i in num:
+    count = 0
+    if i == 1: continue
+    
+    for j in range(2, i+1):
+        if i % j == 0: count += 1
+    
+    if count == 1:
+        num_count += 1
+    
+print(num_count)
