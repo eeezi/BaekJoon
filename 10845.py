ced10032cdb42f39b0b4ce9266b01f1f1cbf5732
eeezi <alloy0035@hanmail.net>
2024-03-13 23:12:07 +0900

    큐

---
+++ 10845.py
@@ -0,0 +1,34 @@
+import sys
+
+n = int(sys.stdin.readline())
+queue = []
+
+for _ in range(n):
+    command = list(sys.stdin.readline().split())
+
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if not queue:
+            print(-1)
+        else:
+            print(queue.pop(0))
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if not queue:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[0])
+    elif command[0] == 'back':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[-1])
+    else:
+        print("command not found")
