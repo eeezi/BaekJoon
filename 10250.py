fe6658a7acdb3c14e268f568141fee7b0e842e8a
eeezi <alloy0035@hanmail.net>
2024-05-09 21:28:40 +0900

    ACM 호텔 - 리팩터링

--- 10250.py
+++ 10250.py
@@ -1,13 +1,16 @@
-t = int(input())
+import sys
 
-for i in range(t):
-    h, w, n = map(int, input().split())
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    h, w, n = map(int, sys.stdin.readline().split())
     
     if(n%h) == 0:
         floor = h
-        room = int(n / h)
+        room = n // h
     else:
         floor = n % h
-        room = int(n / h) + 1
+        room = n // h + 1
 
-    print('%d%02d'%(floor,room),sep='')
+    print('%d%02d'%(floor,room))
+    # print(floor * 100 + room)
